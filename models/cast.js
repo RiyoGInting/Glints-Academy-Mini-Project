baf6473f88d7c8ab15d50ff48f43d85223a0eff9
Azka Farisa <azkafariza@yahoo.com>
2021-04-15 13:11:43 +0700

    cast-done

---
+++ models/cast.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose"); // Import mongoose
+const mongooseDelete = require("mongoose-delete"); // Import mongoose-delete
+
+const CastSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: false,
+      default: null,
+      get: getImage,
+    },
+  },
+  {
+    // Enable timestamps
+    timestamps: {
+      createdAt: "createdAt",
+      updatedAt: "updatedAt",
+    },
+  }
+);
+
+function getImage(image) {
+  if (!image) {
+    return `/default/profile-picture.jpg`;
+  }
+  return `/images/${image}`;
+}
+
+// Enable soft delete
+CastSchema.plugin(mongooseDelete, { overrideMethods: "all" });
+
+module.exports = mongoose.model("cast", CastSchema); // Export user models
