e2bde05c24e5d3006a8ebccb708a2409281b4a1f
riyo <riyoginting@gmail.com>
2021-04-16 11:39:19 +0700

    authorization

--- middlewares/auth/index.js
+++ middlewares/auth/index.js
@@ -108,3 +108,150 @@ passport.use(
     }
   )
 );
+
+exports.admin = (req, res, next) => {
+  passport.authorize("admin", (err, user, info) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Internal Server Error",
+        error: err.message,
+      });
+    }
+
+    if (!user) {
+      return res.status(403).json({
+        message: info.message,
+      });
+    }
+
+    req.user = user;
+
+    next();
+  })(req, res, next);
+};
+
+passport.use(
+  "admin",
+  new JWTstrategy(
+    {
+      secretOrKey: process.env.JWT_SECRET,
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    },
+    async (token, done) => {
+      try {
+        let userSignIn = await user.findOne({ _id: token.user.id });
+
+        if (userSignIn.role.includes("admin")) {
+          return done(null, token.user);
+        }
+
+        return done(null, false, {
+          message: "You're not authorized",
+        });
+      } catch (e) {
+        return done(null, false, {
+          message: "You're not authorized",
+        });
+      }
+    }
+  )
+);
+
+exports.user = (req, res, next) => {
+  passport.authorize("user", (err, user, info) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Internal Server Error",
+        error: err.message,
+      });
+    }
+
+    if (!user) {
+      return res.status(403).json({
+        message: info.message,
+      });
+    }
+
+    req.user = user;
+
+    next();
+  })(req, res, next);
+};
+
+passport.use(
+  "user",
+  new JWTstrategy(
+    {
+      secretOrKey: process.env.JWT_SECRET,
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    },
+    async (token, done) => {
+      try {
+        let userSignIn = await user.findOne({ _id: token.user.id });
+
+        if (userSignIn.role.includes("user")) {
+          return done(null, token.user);
+        }
+
+        return done(null, false, {
+          message: "You're not authorized",
+        });
+      } catch (e) {
+        return done(null, false, {
+          message: "You're not authorized",
+        });
+      }
+    }
+  )
+);
+
+exports.adminOrUser = (req, res, next) => {
+  passport.authorize("adminOrUser", (err, user, info) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Internal Server Error",
+        error: err.message,
+      });
+    }
+
+    if (!user) {
+      return res.status(403).json({
+        message: info.message,
+      });
+    }
+
+    req.user = user;
+
+    next();
+  })(req, res, next);
+};
+
+passport.use(
+  "adminOrUser",
+  new JWTstrategy(
+    {
+      secretOrKey: process.env.JWT_SECRET,
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    },
+    async (token, done) => {
+      try {
+        let userSignIn = await user.findOne({ _id: token.user.id });
+
+        if (
+          userSignIn.role.includes("user") ||
+          userSignIn.role.includes("admin")
+        ) {
+          return done(null, token.user);
+        }
+
+        return done(null, false, {
+          message: "You're not authorized",
+        });
+      } catch (e) {
+        return done(null, false, {
+          message: "You're not authorized",
+        });
+      }
+    }
+  )
+);
