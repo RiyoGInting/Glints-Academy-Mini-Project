f86623da4976174f537df274b808c88b38e863c7
Karina Kusumadewi <karinasarahk@gmail.com>
2021-04-17 14:31:14 +0700

    update review model

--- models/review.js
+++ models/review.js
@@ -9,7 +9,9 @@ const ReviewSchema = new mongoose.Schema(
         },
         rating: {
             type: Number,
-            required: true,
+            min: 1,
+            max: 5,
+            required: [true, "Please rate between 1 and 5"],
         },
         movieId: {
             type: mongoose.Schema.Types.ObjectId,
@@ -32,6 +34,31 @@ const ReviewSchema = new mongoose.Schema(
 
 // only permits user to submit one review per movie
 ReviewSchema.index({ movie: 1, user:1 }, { unique: true });
+
+
+// // Static method to get average rating
+// ReviewSchema.statics.getAverageRating = async function (movieId) {
+//     const obj = await this.aggregate([
+//       {
+//         $match: { movie: movieId },
+//       },
+//       {
+//         $group: {
+//           _id: "$bootcamp",
+//           averageRating: { $avg: "$rating" },
+//         },
+//       },
+//     ]);
+  
+//     try {
+//       await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+//         averageRating: obj[0].averageRating,
+//       });
+//     } catch (e) {
+//       console.error(e);
+//     }
+//   };
+
 
 // enable soft delete
 ReviewSchema.plugin(mongooseDelete, { overrideMethods: "all" });
