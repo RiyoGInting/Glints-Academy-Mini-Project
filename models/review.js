9502bfecc4b576a494ec28bbe87fd2dc460982a6
Karina Kusumadewi <karinasarahk@gmail.com>
2021-04-17 14:36:16 +0700

    update review model

--- models/review.js
+++ models/review.js
@@ -2,63 +2,72 @@ const mongoose = require("mongoose"); // import mongoose
 const mongooseDelete = require("mongoose-delete"); // Import mongoose-delete
 
 const ReviewSchema = new mongoose.Schema(
-    {
-        review: {
-            type: String,
-            required: true,
-        },
-        rating: {
-            type: Number,
-            min: 1,
-            max: 5,
-            required: [true, "Please rate between 1 and 5"],
-        },
-        movieId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "movie",
-            required: true,
-        },
-        userId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "user",
-            required: true,
-        },
+  {
+    review: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: {
-            createdAt: "createdAt",
-            updatedAt: "updatedAt",
-        }
-    }
-)
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, "Please rate between 1 and 5"],
+    },
+    movieId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "movie",
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "createdAt",
+      updatedAt: "updatedAt",
+    },
+  }
+);
 
 // only permits user to submit one review per movie
-ReviewSchema.index({ movie: 1, user:1 }, { unique: true });
+ReviewSchema.index({ movie: 1, user: 1 }, { unique: true });
 
+// Static method to get average rating
+ReviewSchema.statics.getAverageRating = async function (movieId) {
+  const obj = await this.aggregate([
+    {
+      $match: { movie: movieId },
+    },
+    {
+      /*fix this */
+      $group: {
+        _id: "$movie",
+        averageRating: { $avg: "$rating" },
+      },
+    },
+  ]);
 
-// // Static method to get average rating
-// ReviewSchema.statics.getAverageRating = async function (movieId) {
-//     const obj = await this.aggregate([
-//       {
-//         $match: { movie: movieId },
-//       },
-//       {
-//         $group: {
-//           _id: "$bootcamp",
-//           averageRating: { $avg: "$rating" },
-//         },
-//       },
-//     ]);
-  
-//     try {
-//       await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
-//         averageRating: obj[0].averageRating,
-//       });
-//     } catch (e) {
-//       console.error(e);
-//     }
-//   };
+  try {
+    await this.model("Movie").findByIdAndUpdate(movieId, {
+      averageRating: obj[0].averageRating,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
 
+// call getAverageRating after posting review
+ReviewSchema.post("save", function () {
+  this.constructor.getAverageRating(this.movie);
+});
+
+// call getAverageRating after deleting review
+ReviewSchema.pre("remove", function () {
+  this.constructor.getAverageRating(this.movie);
+});
 
 // enable soft delete
 ReviewSchema.plugin(mongooseDelete, { overrideMethods: "all" });
